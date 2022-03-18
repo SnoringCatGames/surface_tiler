25b5524ee42ff56d40884029b9845ee2a09df76f
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2022-03-18 09:45:56 -0700

    Create a reusable FrameworkPlugin parent class.

---
+++ src/plugin/surface_tiler_plugin.gd
@@ -0,0 +1,34 @@
+tool
+class_name SurfaceTilerPlugin
+extends FrameworkPlugin
+
+
+const _DISPLAY_NAME := "SurfaceTiler"
+const _ICON_DIRECTORY_PATH := "res://addons/surface_tiler/assets/images/"
+const _AUTO_LOAD_NAME := "St"
+const _AUTO_LOAD_PATH := "res://addons/surface_tiler/src/config/st.gd"
+const _MAIN_PANEL_SCENE := \
+        preload("res://addons/surface_tiler/src/plugin/surface_tiler_main_panel.tscn")
+
+var _corner_match_tilemap_inspector_plugin: CornerMatchTilemapInspectorPlugin
+
+
+func _init().(
+        _DISPLAY_NAME,
+        _ICON_DIRECTORY_PATH,
+        _AUTO_LOAD_NAME,
+        _AUTO_LOAD_PATH,
+        _MAIN_PANEL_SCENE) -> void:
+    pass
+
+
+func _set_up() -> void:
+    ._set_up()
+    
+    _corner_match_tilemap_inspector_plugin = \
+            CornerMatchTilemapInspectorPlugin.new()
+    add_inspector_plugin(_corner_match_tilemap_inspector_plugin)
+
+
+func _exit_tree() -> void:
+    remove_inspector_plugin(_corner_match_tilemap_inspector_plugin)
