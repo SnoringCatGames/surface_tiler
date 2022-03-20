6c8f5794340a613341b46d75cc9be77cfe86972c
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2022-03-19 22:06:39 -0700

    Convert schema to use type inference.

--- src/plugin/surface_tiler_schema.gd
+++ src/plugin/surface_tiler_schema.gd
@@ -11,39 +11,39 @@ const _AUTO_LOAD_PATH := "res://addons/surface_tiler/src/config/st.gd"
 const _PLUGIN_ICON_DIRECTORY_PATH := "res://addons/surface_tiler/assets/images/editor_icons/"
 
 const _PROPERTIES := {
-    outer_autotile_name = [TYPE_STRING, "autotile"],
-    inner_autotile_name = [TYPE_STRING, "__inner_autotile__"],
-    forces_convex_collision_shapes = [TYPE_BOOL, true],
-    allows_fallback_corner_matches = [TYPE_BOOL, true],
-    supports_runtime_autotiling = [TYPE_BOOL, true],
+    outer_autotile_name = "autotile",
+    inner_autotile_name = "__inner_autotile__",
+    forces_convex_collision_shapes = true,
+    allows_fallback_corner_matches = true,
+    supports_runtime_autotiling = true,
     
-    corner_type_annotation_key_path = [TYPE_STRING,
-        "res://addons/surface_tiler/assets/images/corner_type_annotation_key.png"],
-    implicit_quadrant_connection_color = [TYPE_COLOR, Color("ff3333")],
+    corner_type_annotation_key_path = \
+        "res://addons/surface_tiler/assets/images/corner_type_annotation_key.png",
+    implicit_quadrant_connection_color = Color("ff3333"),
     
-    annotations_parser_class = [TYPE_SCRIPT, preload(
-        "res://addons/surface_tiler/src/calculators/tileset_annotations_parser.gd")],
-    corner_calculator_class = [TYPE_SCRIPT, preload(
-        "res://addons/surface_tiler/src/calculators/subtile_target_corner_calculator.gd")],
-    quadrant_calculator_class = [TYPE_SCRIPT, preload(
-        "res://addons/surface_tiler/src/calculators/subtile_target_quadrant_calculator.gd")],
-    initializer_class = [TYPE_SCRIPT, preload(
-        "res://addons/surface_tiler/src/calculators/corner_match_tileset_initializer.gd")],
-    shape_calculator_class = [TYPE_SCRIPT, preload(
-        "res://addons/surface_tiler/src/calculators/corner_match_tileset_shape_calculator.gd")],
+    annotations_parser_class = preload(
+        "res://addons/surface_tiler/src/calculators/tileset_annotations_parser.gd"),
+    corner_calculator_class = preload(
+        "res://addons/surface_tiler/src/calculators/subtile_target_corner_calculator.gd"),
+    quadrant_calculator_class = preload(
+        "res://addons/surface_tiler/src/calculators/subtile_target_quadrant_calculator.gd"),
+    initializer_class = preload(
+        "res://addons/surface_tiler/src/calculators/corner_match_tileset_initializer.gd"),
+    shape_calculator_class = preload(
+        "res://addons/surface_tiler/src/calculators/corner_match_tileset_shape_calculator.gd"),
     
     tilesets = [
         {
             recalculate_tileset = [TYPE_CUSTOM, RecalculateTilesetCustomProperty],
-            tileset_quadrants_path = [TYPE_STRING,
-                "res://addons/surface_tiler/assets/images/tileset_quadrants.png"],
-            tileset_corner_type_annotations_path = [TYPE_STRING,
-                "res://addons/surface_tiler/assets/images/tileset_corner_type_annotations.png"],
+            tileset_quadrants_path = \
+                "res://addons/surface_tiler/assets/images/tileset_quadrants.png",
+            tileset_corner_type_annotations_path = \
+                "res://addons/surface_tiler/assets/images/tileset_corner_type_annotations.png",
             tile_set = [TYPE_TILESET, null],
-            quadrant_size = [TYPE_INT, 16],
-            subtile_collision_margin = [TYPE_REAL, 3.0],
-            are_45_degree_subtiles_used = [TYPE_BOOL, true],
-            are_27_degree_subtiles_used = [TYPE_BOOL, false],
+            quadrant_size = 16,
+            subtile_collision_margin = 3.0,
+            are_45_degree_subtiles_used = true,
+            are_27_degree_subtiles_used = false,
         },
     ],
 }
