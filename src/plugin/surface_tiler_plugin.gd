5cac121fac36873f7d027ead5aed020dac94162c
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2022-03-23 10:02:58 -0700

    Move plugin files into a separate subdirectory; Create a new autoload for plugin-specific stuff; Decouple FrameworkPlugin from any transitive dependencies on FrameworkGlobal types.

--- src/plugin/surface_tiler_plugin.gd
+++ src/plugin/surface_tiler_plugin.gd
@@ -3,20 +3,31 @@ class_name SurfaceTilerPlugin
 extends FrameworkPlugin
 
 
-const _SCHEMA_CLASS := SurfaceTilerSchema
+const _AUTO_LOAD_NAME := "St"
+const _AUTO_LOAD_PATH := "res://addons/surface_tiler/src/config/st.gd"
+const _SCHEMA_PATH := \
+        "res://addons/surface_tiler/src/plugin/surface_tiler_schema.gd"
 
-var _corner_match_tilemap_inspector_plugin: CornerMatchTilemapInspectorPlugin
+const _CORNER_MATCH_TILEMAP_INSPECTOR_PLUGIN := \
+        "res://addons/surface_tiler/src/plugin/corner_match_tilemap_inspector_plugin.gd"
+
+var _corner_match_tilemap_inspector_plugin
 
 
-func _init().(_SCHEMA_CLASS) -> void:
+func _init().(
+        _AUTO_LOAD_NAME,
+        _AUTO_LOAD_PATH,
+        _SCHEMA_PATH) -> void:
     pass
 
 
 func _set_up() -> void:
     ._set_up()
     
+    # FIXME: LEFT OFF HERE: ------------
+    # - Can I preload this without circular deps?
     _corner_match_tilemap_inspector_plugin = \
-            CornerMatchTilemapInspectorPlugin.new()
+            load(_CORNER_MATCH_TILEMAP_INSPECTOR_PLUGIN).new()
     add_inspector_plugin(_corner_match_tilemap_inspector_plugin)
 
 
