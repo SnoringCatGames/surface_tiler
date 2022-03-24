e907503add764f3053c965b22d20ab6c17541319
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2022-03-24 09:52:04 -0700

    Update plugin metadata to support icons in different directories.

--- src/plugin/surface_tiler_metadata.gd
+++ src/plugin/surface_tiler_metadata.gd
@@ -3,13 +3,13 @@ class_name SurfaceTilerMetadata
 extends PluginMetadata
 
 
-const DISPLAY_NAME := "Surface_tiler"
+const DISPLAY_NAME := "Surface Tiler"
 const FOLDER_NAME := "surface_tiler"
 const AUTO_LOAD_NAME := "St"
 const AUTO_LOAD_DEPS := ["Sc"]
 const AUTO_LOAD_PATH := "res://addons/surface_tiler/src/config/st.gd"
-const PLUGIN_ICON_DIRECTORY_PATH := \
-        "res://addons/surface_tiler/assets/images/editor_icons/"
+const PLUGIN_ICON_PATH_PREFIX := \
+        "res://addons/surface_tiler/assets/images/editor_icons/surface_tiler"
 const SCHEMA_PATH := \
         "res://addons/surface_tiler/src/plugin/surface_tiler_schema.gd"
 const MANIFEST_PATH := "res://addons/surface_tiler/src/config/manifest.json"
@@ -21,7 +21,7 @@ func _init().(
         AUTO_LOAD_NAME,
         AUTO_LOAD_DEPS,
         AUTO_LOAD_PATH,
-        PLUGIN_ICON_DIRECTORY_PATH,
+        PLUGIN_ICON_PATH_PREFIX,
         SCHEMA_PATH,
         MANIFEST_PATH) -> void:
     pass
